f03bdc9fea4ba99d840167b96affc470e7cca5f6
MdMuzahid07 <mdmuzahid7396@gmail.com>
2024-06-19 10:58:13 +0600

    cookie parser install and using in app.use

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,14 @@ import cors from 'cors'
 import { UserRoutes } from './app/modules/user/user.routes';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import NotFound from './app/middlewares/notFound';
+import cookieParser from "cookie-parser";
 import { LoginRoute } from './app/modules/auth/auth.routes';
 const app: Application = express();
 
 // parsers
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 // application routes
 app.use('/api', UserRoutes);
